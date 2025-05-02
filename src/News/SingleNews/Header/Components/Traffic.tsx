3fd01e9808dcac64322ac4011ae1928fbbf11a07
RenataGaraeva <renatagaraeva12@gmail.com>
2025-05-02 23:11:27 +0300

    создала кусочек Traffic (траффик из стран)

---
+++ src/News/SingleNews/Header/Components/Traffic.tsx
@@ -0,0 +1,23 @@
+import { Flex, Typography } from "antd";
+import { IData_TrafficItem } from "../../../../app/Types.tsx";
+
+interface Props {
+  traffic: IData_TrafficItem[];
+}
+export default function Traffic ({traffic}: Props) {
+  const getPercent = (count: number) => Math.floor(count * 100) + '%';
+
+  return (
+    <>
+    <Typography.Text type={"secondary"}>Top Traffic:</Typography.Text>
+  {traffic.map((traffic, index) => (
+
+
+    <Flex gap={5} wrap={true} key={index}>
+      {traffic.value}
+      <Typography>{getPercent(traffic.count)}</Typography>
+    </Flex>
+  ))}
+    </>
+  )
+}
--- src/News/SingleNews/Header/Header.tsx
+++ src/News/SingleNews/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { IData_SnippetNews } from "../../../app/Types.tsx";
 import DATE from "./Components/Date.tsx";
 import Reach from "./Components/Reach.tsx";
+import Traffic from "./Components/Traffic.tsx";
 
 interface Props {
   news: IData_SnippetNews
@@ -12,6 +13,7 @@ export default function Header ({news}: Props) {
     <>
    <DATE newsDate={news.DP} />
       <Reach reach={news.REACH}/>
+      <Traffic traffic={news.TRAFFIC}/>
     </>
   )
 }
