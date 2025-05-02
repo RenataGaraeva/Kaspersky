da040ec64b83c6336fba4230d170a588f4b0cdc1
RenataGaraeva <renatagaraeva12@gmail.com>
2025-05-03 02:57:15 +0300

    добавила стили для подложки для новостей

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import News from './News/News'
 
 function App() {
--- src/News/News.tsx
+++ src/News/News.tsx
@@ -1,14 +1,24 @@
 import {Col, Flex, Row} from "antd";
 import SingleNews from "./SingleNews/SingleNews.tsx";
 import { IData_SnippetNews } from "../app/Types.tsx";
-import { useAppSelector } from "../API/hooks.tsx";
-import { selectAllNews } from "../API/NewsSlice.tsx";
+import { useAppDispatch, useAppSelector } from "../API/hooks.tsx";
+import { fetchPosts, selectAllNews, selectNewsStatus } from "../API/NewsSlice.tsx";
+import { useEffect } from "react";
 
 export default function News() {
+  const dispatch = useAppDispatch()
   const news = useAppSelector(selectAllNews)
+  const newsStatus = useAppSelector(selectNewsStatus)
+
+
+  useEffect(() => {
+    if (newsStatus === 'idle') {
+      dispatch(fetchPosts())
+    }
+  }, [newsStatus, dispatch])
   return (
 
-    <Row justify={"center"} align={"middle"}>
+    <Row justify={"center"} align={"middle"} >
       <Col xs={22} sm={22} md={22} lg={16} xl={12}>
         <Flex vertical={true} wrap={true} align={'center'} >
           {news.map((news: IData_SnippetNews) => (
