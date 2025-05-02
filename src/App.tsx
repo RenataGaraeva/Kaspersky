69dc0233f6dabd56232872b1fe98c50c26849c65
RenataGaraeva <renatagaraeva12@gmail.com>
2025-05-03 02:58:08 +0300

    добавила базовые стили

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import News from './News/News'
+import { ConfigProvider} from "antd";
 
 function App() {
 
   return (
     <>
-      <News/>
+      <ConfigProvider
+        theme={{
+          components: {
+            Typography: {
+              colorText: 'white',
+              colorTextDescription: 'grey',
+              colorLink: '#1890ff',
+
+            },
+            Tag: {
+              defaultBg: 'black',
+              colorBorder: 'grey',
+              borderRadiusSM: 12
+
+            },
+            Card: {
+              colorBgContainer: 'black'
+            }
+          }
+        }}
+      >
+        <News/>
+      </ConfigProvider>
     </>
   )
 }
