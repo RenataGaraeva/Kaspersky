b2feec940e610dbbe8b39b5444f28f4f7571e977
RenataGaraeva <renatagaraeva12@gmail.com>
2025-05-03 01:59:47 +0300

    создала кусочек для ключевых слов

---
+++ src/News/SingleNews/Footer/Footer_Component/Keywords.tsx
@@ -0,0 +1,21 @@
+import {Flex, Tag, Typography} from "antd";
+import { IData_SnippetNews, IData_TagItem } from "../../../../app/Types.tsx";
+
+interface KeywordsProps {
+  news: IData_SnippetNews;
+}
+
+export default function Keywords({ news }: KeywordsProps) {
+
+  return (
+    <Flex wrap={true} gap="small"  >
+      {news.KW.map((keywords: IData_TagItem) => (
+        <Tag style={{display: "flex"}}>
+          <Typography.Text type={"secondary"} style={{marginRight: '5px'}}>{keywords.value}</Typography.Text>
+          <Typography>{keywords.count}</Typography>
+        </Tag>
+      ))}
+    </Flex>
+
+  );
+}
